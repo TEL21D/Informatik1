914ef0ce26c22973c436780cde3d8601615e6caa
toj11001 <tobias.joschko@gmx.de>
2021-12-07 19:17:19 +0100

    update 07.12

--- Vorlesungsmaterial/21-12-07/conditionals.cpp
+++ Vorlesungsmaterial/21-12-07/conditionals.cpp
@@ -1,9 +1,28 @@
 #include "header.hpp"
-
+#define HIGH 1
+#define LOW 0
 
 int main(int argc, char const *argv[])
 {
-   bool wahr =  true, falsch = false;
+   bool wahr = true, falsch = false;
+   int input = 0;
+   while (wahr)
+   {
+      if (HIGH)
+      {
+         cout << "if \n";
+      }
+      else
+      {
+         cout << "else \n";
+      }
+      cout << "1. Zum beenden\n2. Weiterlaufen\n";
+      cin >> input;
+      if(input == 1) {
+         cout << "Programm beendet\n";
+         wahr = !wahr;
+      }
+   }
 
-   return 0;
+   return EXIT_SUCCESS;
 }
--- Vorlesungsmaterial/21-12-07/header.hpp
+++ Vorlesungsmaterial/21-12-07/header.hpp
@@ -1,9 +1,9 @@
 #ifndef HEADER_HPP
 #define HEADER_HPP
 
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 #endif /* HEADER_HPP */
