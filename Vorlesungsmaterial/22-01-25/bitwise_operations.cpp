86a53e1dc2f46aeb059d91a72c18d142440c9588
toj11001 <tobias.joschko@gmx.de>
2022-01-25 19:49:22 +0100

    Vorlesung 25.01

---
+++ Vorlesungsmaterial/22-01-25/bitwise_operations.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+   // 1000
+   int a = 8;
+   // 0100
+   int b = 4;
+   // 1100
+   int c = 12;
+   int d = a & b;
+   int e = a & c;
+   int f = b & c;
+   std::cout << "1000 & 0100: " << d << std::endl;
+   std::cout << "1000 & 1100: " << e << std::endl;
+   std::cout << "0100 & 1100: " << f << std::endl;
+
+   d = a | b;
+   std::cout << "1000 | 0100: " << d << std::endl;
+
+   return 0;
+}
---
+++ Vorlesungsmaterial/22-01-25/range_based_loops.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+int main(int argc, char const *argv[])
+{
+   std::vector<std::string> vec1 = {
+      "Zeile1",
+      "Zeile2",
+      "Zeile3"
+   };
+
+   // Range basierte loops
+   // auto vergibt automatisch den Typ
+   for (auto zeile : vec1)
+   {
+      // 0: zeile = vec[0]
+      // 1: zeile = vec[1]
+      // 2: zeile = vec[2]
+      // .....
+      // n-1: zeile = vec[n-1]
+      std::cout << zeile << std::endl;
+      for (auto c : zeile){
+         std::cout << c << std::endl;
+      }
+   }
+
+   std::cout << std::endl;
+
+   // Manuell der Typ auf String setzen
+   for (std::string zeile : vec1)
+   {
+      std::cout << zeile << std::endl;
+   }
+
+
+   return 0;
+}
