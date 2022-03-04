9252a18c6cf9ebd7ca1887374aa390aa3e8f8969
toj11001 <tobias.joschko@gmx.de>
2022-03-04 11:56:29 +0100

    update foo aufgabe

--- Aufgaben/Laboraufgaben_WS1920/04_schleifen2.cpp
+++ Aufgaben/Laboraufgaben_WS1920/04_schleifen2.cpp
@@ -3,17 +3,15 @@
 /*** AUFGABE: Schleifen, 6 Punkte ***/
 
 /*** AUFGABENSTELLUNG:
-     
+
      Betrachten Sie die unten vorgegebene Funktion foo().
      Schreiben Sie eine Funktion bar() mit den folgenden
 	 Eigenschaften:
-     
-     Für x >= 0 soll gelten: bar(foo(x)) == x.
-     Für x < 0 soll gelten: bar(foo(x)) == -x.
-     
-     D.h. für positive x soll bar() den Effekt von foo()
-     umkehren, für negative x soll sie das entsprechende
-     negative Ergebnis liefern.
+
+     FÃ¼r x >= 0 soll gelten: bar(foo(x)) == x.
+
+     D.h. fÃ¼r positive x soll bar() den Effekt von foo()
+     umkehren.
 ***/
 int bar(int x);
 
@@ -26,13 +24,11 @@ int foo(int x)
 
 /*** TESTCODE/MAIN: ***/
 int main() {
-    
+
     printf("%d\n",bar(foo(1)));           // Soll 1 ausgeben
     printf("%d\n",bar(foo(42)));          // Soll 42 ausgeben
     printf("%d\n",bar(foo(-2)));          // Soll 2 ausgeben
     printf("%d\n",bar(foo(0)));           // Soll 0 ausgeben
-      
+
     return 0;
 }
-
-
